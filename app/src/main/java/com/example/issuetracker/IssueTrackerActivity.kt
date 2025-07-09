da0bba6f6d82596c38fe53761b7fefd71a635af7
suvojit213 <suvojitsengupta21@gmail.com>
2025-07-09 13:51:04 +0530

    Feat: Implement IssueTrackerActivity and navigation from MainActivity

---
+++ app/src/main/java/com/example/issuetracker/IssueTrackerActivity.kt
@@ -0,0 +1,12 @@
+package com.example.issuetracker
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class IssueTrackerActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_issue_tracker)
+    }
+}
--- app/src/main/java/com/example/issuetracker/MainActivity.kt
+++ app/src/main/java/com/example/issuetracker/MainActivity.kt
@@ -1,11 +1,15 @@
 package com.example.issuetracker
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val intent = Intent(this, IssueTrackerActivity::class.java)
+        startActivity(intent)
+        finish()
+        
     }
 }
